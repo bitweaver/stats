19630dd3f0c7de51cebfa85e322436f81666bd4f
Lester Caine <lester@lsces.co.uk>
2005-06-27 15:56:42 +0000

    Adding DocBlock elements to improve phpDocumentor output

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_stats/index.php,v 1.1.1.1.2.1 2005/06/27 15:56:42 lsces Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: index.php,v 1.1.1.1.2.1 2005/06/27 15:56:42 lsces Exp $
+ * @package stats
+ * @subpackage functions
+ */
 
-// $Header: /cvsroot/bitweaver/_bit_stats/index.php,v 1.1 2005/06/19 05:05:49 bitweaver Exp $
-
-// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-
-// Initialization
+/**
+ * required setup
+ */
 require_once( '../bit_setup_inc.php' );
 
 include_once( STATS_PKG_PATH.'stats_lib.php' );
