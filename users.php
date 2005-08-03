bf8584f4b98f9bb9ef08a29402deaf21f33e1c22
Lester Caine <lester@lsces.co.uk>
2005-08-03 16:53:59 +0000

    Clears all errors reported in phpdoc build Clean build is up on http://home.lsces.co.uk/bitweaverdocsPHP/

--- users.php
+++ users.php
@@ -1,19 +1,21 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_stats/users.php,v 1.1.2.2 2005/07/26 15:50:28 drewslater Exp $
+ * $Header: /cvsroot/bitweaver/_bit_stats/users.php,v 1.1.2.3 2005/08/03 16:53:59 lsces Exp $
  *
  * Copyright (c) 2005 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: users.php,v 1.1.2.2 2005/07/26 15:50:28 drewslater Exp $
+ * $Id: users.php,v 1.1.2.3 2005/08/03 16:53:59 lsces Exp $
  * @package stats
  * @subpackage functions
  */
-
+/**
+ * Required files
+ */
 require_once( '../bit_setup_inc.php' );
-
 include_once( STATS_PKG_PATH.'stats_lib.php' );
+
 global $statslib, $gBitSystem;
 
 $gBitSystem->verifyPackage( 'stats' );
