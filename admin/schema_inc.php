9aef9295fb300b1d46443fda889d5f2da007dddf
Max Kremmel <xing@synapse.plus.com>
2005-10-29 17:56:35 +0000

    merge recent changes into HEAD

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -3,9 +3,9 @@
 $tables = array(
 
 'tiki_referer_stats' => "
-  referer   C(50) NOTNULL,
-  hits		I8, 
-  last  	I8
+	referer C(50) NOTNULL,
+	hits I8, 
+	last I8
 "
 
 );
@@ -31,8 +31,8 @@ $gBitInstaller->registerPreferences( STATS_PKG_NAME, array(
 
 // ### Default UserPermissions
 $gBitInstaller->registerUserPermissions( STATS_PKG_NAME, array(
-	array('bit_p_view_referer_stats', 'Can view referer stats', 'editors', 'stats'),
-	array('bit_p_view_stats', 'Can view site stats', 'basic', 'stats'),
+	array('bit_p_view_referer_stats', 'Can view referer stats', 'editors', STATS_PKG_NAME),
+	array('bit_p_view_stats', 'Can view site stats', 'basic', STATS_PKG_NAME),
 ) );
 
 
