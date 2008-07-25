5b01439c5815377e01aeb929e5fd143c1ce08807
Stephan Borg <wolff_borg@users.sourceforge.net>
2008-07-25 14:57:46 +0000

    Changed to bar and non-logarithmic graphs. Removed the No Data graphic and changed graph zero data.

--- item_chart.php
+++ item_chart.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_stats/item_chart.php,v 1.6 2008/06/10 09:03:32 omniscent Exp $
+ * $Header: /cvsroot/bitweaver/_bit_stats/item_chart.php,v 1.7 2008/07/25 14:57:46 wolff_borg Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: item_chart.php,v 1.6 2008/06/10 09:03:32 omniscent Exp $
+ * $Id: item_chart.php,v 1.7 2008/07/25 14:57:46 wolff_borg Exp $
  * @package stats
  * @subpackage functions
  */
@@ -27,37 +27,30 @@ $gBitSystem->verifyPermission( 'p_stats_view' );
 $stats = new Statistics();
 
 $data = $stats->getContentTypeChartData( !empty( $_REQUEST['content_type_guid'] ) ? $_REQUEST['content_type_guid'] : NULL );
-$chart_type = !empty( $_REQUEST['chart_type'] ) ? $_REQUEST['chart_type'] : 'points';
+$chart_type = !empty( $_REQUEST['chart_type'] ) ? $_REQUEST['chart_type'] : 'bars';
 
-if( !empty( $_REQUEST['content_type_guid'] ) ) {
-	$data['data'] = $data['data'][$_REQUEST['content_type_guid']];
-}
 // initialise phplot and insert data
 $graph =& new PHPlot( 600, 400 * ( count( $data['data'] ) ) );
 $graph->SetPrintImage(0);
 $graph->SetPlotType( $chart_type );
 $graph->SetXTickPos( 'none' );
-$graph->SetYScaleType( 'log' );
+//$graph->SetYScaleType( 'log' );
 $graph->SetTitle( tra( $data['title'] ) );
 $graph->SetXLabel( tra( 'Title' ) );
 
 $i = 0;
-if( !empty( $data['data'][0][1] )) {
-	foreach( $data['data'] as $guid => $info ) {
-		$graph->SetDataValues( $info );
-		$graph->SetDrawXDataLabels( TRUE );
-		$graph->SetXLabelAngle( ( count( $info ) > 5 ) ? 90 : 0 );
-		$graph->SetNewPlotAreaPixels( 75, 30 + ( $i * 390 ), 580, 280 + ( $i * 390 ) );
-		if( !empty( $_REQUEST['content_type_guid'] ) ) {
-			$graph->SetYLabel( $gLibertySystem->mContentTypes[$_REQUEST['content_type_guid']]['content_description'].' '.tra( 'Hits' ).' ('.tra( "log" ).')' );
-		} else {
-			$graph->SetYLabel( $gLibertySystem->mContentTypes[$guid]['content_description'].' '.tra( 'Hits' ).' ('.tra( "log" ).')' );
-		}
-		$graph->DrawGraph();
-		$i++;
+foreach( $data['data'] as $guid => $info ) {
+	$graph->SetDataValues( $info );
+	$graph->SetDrawXDataLabels( TRUE );
+	$graph->SetXLabelAngle( ( count( $info ) > 5 ) ? 90 : 0 );
+	$graph->SetNewPlotAreaPixels( 75, 30 + ( $i * 390 ), 580, 370 + ( $i * 390 ) );
+	if( !empty( $_REQUEST['content_type_guid'] ) ) {
+		$graph->SetYLabel( $gLibertySystem->mContentTypes[$_REQUEST['content_type_guid']]['content_description'].' '.tra( 'Hits' ).' ('.tra( "log" ).')' );
+	} else {
+		$graph->SetYLabel( $gLibertySystem->mContentTypes[$guid]['content_description'].' '.tra( 'Hits' ).' ('.tra( "log" ).')' );
 	}
-	$graph->PrintImage();
-} else {
-	include_once( STATS_PKG_PATH.'icons/nodata.png' );
+	$graph->DrawGraph();
+	$i++;
 }
+$graph->PrintImage();
 ?>
