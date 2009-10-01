d0f94e0f111d7ca8f5502ec9b1b7ffa450b9f6a8
wjames5 <will@tekimaki.com>
2009-10-01 14:17:05 +0000

    remove ref to non-existant copyright.txt file

--- usage_chart.php
+++ usage_chart.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_stats/usage_chart.php,v 1.7 2009/10/01 13:45:48 wjames5 Exp $
+ * $Header: /cvsroot/bitweaver/_bit_stats/usage_chart.php,v 1.8 2009/10/01 14:17:05 wjames5 Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
  * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
  *
- * $Id: usage_chart.php,v 1.7 2009/10/01 13:45:48 wjames5 Exp $
+ * $Id: usage_chart.php,v 1.8 2009/10/01 14:17:05 wjames5 Exp $
  * @package stats
  * @subpackage functions
  */
--- users.php
+++ users.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_stats/users.php,v 1.10 2009/10/01 13:45:48 wjames5 Exp $
+ * $Header: /cvsroot/bitweaver/_bit_stats/users.php,v 1.11 2009/10/01 14:17:05 wjames5 Exp $
  *
  * Copyright (c) 2005 bitweaver.org
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
  *
- * $Id: users.php,v 1.10 2009/10/01 13:45:48 wjames5 Exp $
+ * $Id: users.php,v 1.11 2009/10/01 14:17:05 wjames5 Exp $
  * @package stats
  * @subpackage functions
  */
